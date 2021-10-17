5ec242ba604cce72e9f7c86c2b1795086729029d
Federico Rey <federico.s.rey@gmail.com>
2021-10-17 19:11:41 -0300

    funciones no usadas

--- routes/api/user.route.js
+++ routes/api/user.route.js
@@ -1,8 +1,6 @@
 var express = require('express')
 var router = express.Router()
 var UserController = require('../../controllers/users.controller');
-var ContactoController = require('../../controllers/contacto.controller');
-var EncuestaController = require('../../controllers/encuesta.controller');
 var TarjetaController = require('../../controllers/tarjeta.controller');
 var UserTarjetaController = require('../../controllers/usertarjeta.controller');
 var MovimientoController = require('../../controllers/movimiento.controller');
@@ -20,41 +18,33 @@ router.put('/actualizacion', UserController.updateUser)
 router.post('/usr', UserController.removeUser)
 router.post('/getInfoUsuario', UserController.getInfoUsuario)
 
-// Endpoints contacto
-router.post('/contacto', ContactoController.createContacto)
-
 //Endpoint tarjetas
 router.post('/tarjeta', TarjetaController.agregarTarjeta)
 router.get('/getTarjetas', TarjetaController.getTarjetas)
 router.get('/getTarjeta', TarjetaController.getTarjeta)
-
 router.post('/asignarTarjeta', UserTarjetaController.asignarTarjeta)
+
+//Endpoint Movimientos
 router.post('/agregarMovimiento', MovimientoController.agregarMovimiento)
 router.get('/getMovimientos', MovimientoController.getMovimientos)
 router.post('/getNMovimientos', MovimientoController.getNMovimientos)
 router.post('/getUMovimientos', MovimientoController.getUMovimientos)
 router.post('/getMontosaPagaraEstablecimientostcb',MovimientoController.getMontosaPagaraEstablecimientos)
 router.post('/getMontosaCobrarxConsumosClientestcb',MovimientoController.getMontosaCobrarxConsumosClientes)
-
 router.post('/UpdateidLiquidacionMovimiento', MovimientoController.UpdateidLiquidacionMovimiento)
 router.post('/UpdateidPagoMovimiento', MovimientoController.UpdateidPagoMovimiento)
 
+//Endpoint Liquidaciones
 router.post('/postLiquidaciones', LiquidacionController.postLiquidaciones)
 router.post('/getLiquidaciones', LiquidacionController.getLiquidaciones)
 router.post('/UpdateidCobroLiquidacion', LiquidacionController.UpdateidCobroLiquidacion)
 
+//Endpoint Cobros
 router.post('/agregarCobro', CobroController.agregarCobro)
 router.get('/getCobros', CobroController.getCobros)
 
+//Endpoint Pagos
 router.post('/agregarPago', PagoController.agregarPago)
 router.get('/getPagos', PagoController.getPagos)
-
-// Endpoints encuesta
-router.post('/encuesta', EncuestaController.createEncuesta)
-router.get('/tencuesta', EncuestaController.getEncuesta)
-router.post('/bencuesta', EncuestaController.removeEncuesta)
-router.post('/encuestaid', EncuestaController.getEncuestaID)
-router.post('/encuestarepid', EncuestaController.getEncuestaRespID)
-router.post('/encuestaResp', EncuestaController.createEncuestaResp)
 
 module.exports = router;
